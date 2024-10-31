06c48336eabfe78d9e4f5240464e9abc1539c306
johnnyw <jwu38438@gmail.com>
2024-10-31 13:57:29 -0400

    cvwpeo[wppvwwwwewvwvw

--- sigma/JS/main.js
+++ sigma/JS/main.js
@@ -7,14 +7,22 @@ const DOMselectors =
 DOMselectors.button.addEventListener("change", function(event)
 {
     const body =  document.querySelector("body")
+    const slider =  document.querySelector(".slider")
     if(body.classList.contains("dark")){
         body.classList.add("light")
         body.classList.remove("dark")
+        slider.classList.add("activeon")
+        slider.classList.remove("activeoff")
+
     }
     else if(body.classList.contains("light"))
     {
         body.classList.remove("light")
         body.classList.add("dark")
+        slider.classList.remove("activeon")
+        slider.classList.add("activeoff")
+
     }
 
-} )
+    }
+)
