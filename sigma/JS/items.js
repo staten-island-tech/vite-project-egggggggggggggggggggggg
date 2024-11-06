7a847ff270c88957fb724df31d65d66cd5f3cac0
johnnyw <jwu38438@gmail.com>
2024-11-06 13:56:53 -0500

    egegegeg

--- sigma/JS/items.js
+++ sigma/JS/items.js
@@ -3,120 +3,142 @@ const pokemon = [
         "name": "Flareon",
         "type": ["Fire"],
         "region": "Kanto",
-        "evolution": ["Eevee"]
+        "evolution": ["Eevee"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Gardevoir",
         "type": ["Psychic", "Fairy"],
         "region": "Hoenn",
-        "evolution": ["Ralts", "Kirlia"]
+        "evolution": ["Ralts", "Kirlia"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Charizard",
         "type": ["Fire", "Flying"],
         "region": "Kanto",
-        "evolution": ["Charmander", "Charmeleon"]
+        "evolution": ["Charmander", "Charmeleon"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Lucario",
         "type": ["Fighting", "Steel"],
         "region": "Sinnoh",
-        "evolution": ["Riolu"]
+        "evolution": ["Riolu"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Torterra",
         "type": ["Grass", "Ground"],
         "region": "Sinnoh",
-        "evolution": ["Turtwig", "Grotle"]
+        "evolution": ["Turtwig", "Grotle"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png"  // empty array for photos
     },
     {
         "name": "Gyarados",
         "type": ["Water", "Flying"],
         "region": "Kanto",
-        "evolution": ["Magikarp"]
+        "evolution": ["Magikarp"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/002.png"  // empty array for photos
     },
     {
         "name": "Mimikyu",
         "type": ["Ghost", "Fairy"],
         "region": "Alola",
-        "evolution": []
+        "evolution": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png",
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Incineroar",
         "type": ["Fire", "Dark"],
         "region": "Alola",
-        "evolution": ["Litten", "Torracat"]
+        "evolution": ["Litten", "Torracat"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Dragonite",
         "type": ["Dragon", "Flying"],
         "region": "Kanto",
-        "evolution": ["Dratini", "Dragonair"]
+        "evolution": ["Dratini", "Dragonair"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Corviknight",
         "type": ["Flying", "Steel"],
         "region": "Galar",
-        "evolution": ["Rookidee", "Corvisquire"]
+        "evolution": ["Rookidee", "Corvisquire"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Ampharos",
         "type": ["Electric"],
         "region": "Johto",
-        "evolution": ["Mareep", "Flaaffy"]
+        "evolution": ["Mareep", "Flaaffy"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Sableye",
         "type": ["Dark", "Ghost"],
         "region": "Hoenn",
-        "evolution": []
+        "evolution": [],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Venusaur",
         "type": ["Grass", "Poison"],
         "region": "Kanto",
-        "evolution": ["Bulbasaur", "Ivysaur"]
+        "evolution": ["Bulbasaur", "Ivysaur"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Froslass",
         "type": ["Ice", "Ghost"],
         "region": "Sinnoh",
-        "evolution": ["Snorunt"]
+        "evolution": ["Snorunt"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Scizor",
         "type": ["Bug", "Steel"],
         "region": "Johto",
-        "evolution": ["Scyther"]
+        "evolution": ["Scyther"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Hatterene",
         "type": ["Psychic", "Fairy"],
         "region": "Galar",
-        "evolution": ["Hattrem", "Hattenna"]
+        "evolution": ["Hattrem", "Hattenna"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Togekiss",
         "type": ["Fairy", "Flying"],
         "region": "Sinnoh",
-        "evolution": ["Togepi", "Togetic"]
+        "evolution": ["Togepi", "Togetic"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Gallade",
         "type": ["Psychic", "Fighting"],
         "region": "Sinnoh",
-        "evolution": ["Ralts", "Kirlia"]
+        "evolution": ["Ralts", "Kirlia"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Cinderace",
         "type": ["Fire"],
         "region": "Galar",
-        "evolution": ["Scorbunny", "Raboot"]
+        "evolution": ["Scorbunny", "Raboot"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     },
     {
         "name": "Porygon-Z",
         "type": ["Normal"],
         "region": "Sinnoh",
-        "evolution": ["Porygon", "Porygon2"]
+        "evolution": ["Porygon", "Porygon2"],
+        "photos": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png"  // empty array for photos
     }
 ];
+
+export { pokemon };
--- sigma/JS/main.js
+++ sigma/JS/main.js
@@ -1,10 +1,86 @@
 import '../css/style.css'
-const DOMselectors = 
+import {pokemon} from "../JS/items.js"
+console.log(pokemon);
+const DOMSelectors = 
 {
-        button: document.querySelector(".switch input")
-
+        button: document.querySelector(".switch input"),
+        container: document.querySelector(".container"),
+        form: document.querySelector("form")
 }
-DOMselectors.button.addEventListener("change", function(event)
+
+pokemon.forEach(stats=>
+    {
+        DOMSelectors.container.insertAdjacentHTML(
+            "afterbegin",
+            `<div class="card">
+              <h2 class="card_header">${stats["name"]}</h2>
+              <img src="${stats["photos"]}" alt="${stats["name"]}" class="card_img">
+              <h2 class="card_header">${stats["type"]}</h2>
+              <h2 class="card_header">${stats["evolution"]}</h2> 
+            </div>`
+          );
+    }
+)
+
+DOMSelectors.container.addEventListener("click", function (event) {
+  const element_selected = event.target.closest(".card");
+  if (!element_selected) return; 
+  const handleMouseDown = () => {
+    const message = document.createElement("div");
+    message.className = "delete-message";
+    message.innerText = "Hold down for 2 seconds to delete";
+    DOMSelectors.container.appendChild(message);
+    message.style.position = "absolute";
+    message.style.top = `${element_selected.getBoundingClientRect().top}px`;
+    message.style.left = `${element_selected.getBoundingClientRect().left}px`;
+
+    const ItemFadeOut = setTimeout(() => {
+      const allmessages = document.querySelectorAll(".delete-message");
+      allmessages.forEach((message) => 
+        message.classList.add("fade-out"),
+        message.remove());
+    }, 1000);
+
+    element_selected.classList.add("holding");
+    const animations = [
+      { time: 500, animation: "shaking 0.25s infinite" },
+      { time: 1000, animation: "shaking 0.125s infinite" },
+      { time: 1500, animation: "shaking 0.09s infinite" },
+    ];
+
+    const timers = animations.map(({ time, animation }) =>
+      setTimeout(() => (element_selected.style.animation = animation), time)
+    );
+
+    const deleteElement = setTimeout(() => {
+      element_selected.remove();
+    }, 2000);
+
+    const clearTimers = () => {
+      timers.forEach(clearTimeout);
+      clearTimeout(deleteElement);
+      element_selected.classList.remove("holding");
+      element_selected.style.animation = "";
+    };
+
+    element_selected.addEventListener("mouseleave", clearTimers);
+    element_selected.addEventListener("mouseup", clearTimers);
+  };
+
+  element_selected.addEventListener("mousedown", handleMouseDown, {
+    once: true,
+  });
+});
+DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.querySelector(".Name").value;
+    const image = document.querySelector(".IMAGE_URL").value;
+    if(!name||!image){
+        console.log("INPUT SOMETHING")
+    }
+    
+})
+DOMSelectors.button.addEventListener("change", function(event)
 {
     const body =  document.querySelector("body")
     const slider =  document.querySelector(".slider")
